7d28d93b0245027f3d231e25e33fbc2a2b2c531f
alibalishka <tokhtarbayali@gmail.com>
2026-04-01 11:11:47 +0500

    Remove Yandex Maps API key and comment out TalkerDioLogger in DioSettings for improved code clarity and organization.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,6 +1,5 @@
 import UIKit
 import Flutter
-import YandexMapsMobile
 // import app_links
 
 @main
@@ -9,9 +8,6 @@ import YandexMapsMobile
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Установка API ключа для Yandex Maps
-    YMKMapKit.setApiKey("68428e34-3d85-439d-bc8b-d5f8f85b89b6")
-
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
